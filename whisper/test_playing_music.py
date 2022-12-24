55a650f4ac80f29c2063448c4fa479ae008c20d9
yagy <logi_st@outlook.jp>
2022-12-25 08:02:51 +0900

    test playing music

---
+++ whisper/test_playing_music.py
@@ -0,0 +1,5 @@
+from playsound import playsound
+
+audio_file = "./materials/music.mp3"
+
+playsound(audio_file)  
--- whisper/test_whisperx.py
+++ whisper/test_whisperx.py
@@ -1,10 +1,11 @@
 import whisperx
+from playsound import playsound
 
 device = "cuda:0"
 audio_file = "./materials/music.mp3"
 
 # transcribe with original whisper
-model = whisperx.load_model("tiny", device)
+model = whisperx.load_model("small", device)
 result = model.transcribe(audio_file, fp16=False)
 
 # load alignment model and metadata
@@ -13,7 +14,12 @@ model_a, metadata = whisperx.load_align_model(language_code=result["language"], 
 # align whisper output
 result_aligned = whisperx.align(result["segments"], model_a, metadata, audio_file, device)
 
-print(result["segments"]) # before alignment
+# print(result["segments"]) # before alignment
 
-print(result_aligned["segments"]) # after alignment
-print(result_aligned["word_segments"]) # after alignment
+# print(result_aligned["segments"]) # after alignment
+# print(result_aligned["word_segments"]) # after alignment
+
+for segment in result_aligned["word_segments"]:
+  print(f'{segment["text"]}, {segment["start"]}')
+  
+playsound(audio_file)
